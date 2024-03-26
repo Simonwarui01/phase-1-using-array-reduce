35b84146f7801e68c2de32ff5fc7c3ef66f54267
Simon <simonwarui93@gmail.com>
2024-03-27 00:57:25 +0300

    assigment

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+// index.js
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+// Calculate the total number of batteries
+const totalBatteries = batteryBatches.reduce((acc, curr) => acc + curr, 0);
+
+// Export the totalBatteries variable
+module.exports = {
+  totalBatteries
+};
